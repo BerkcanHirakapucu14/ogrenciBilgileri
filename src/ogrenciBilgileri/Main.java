08db726f4d6916a49bbb87f1d7a492ad74d13a5d
berkcan14 <berkcanhirakapucu@yandex.com>
2021-10-12 16:28:41 +0300

    Initial commit

---
+++ src/ogrenciBilgileri/Main.java
@@ -0,0 +1,15 @@
+package ogrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Sytem.out.println("Adým :Berkcan");
+        System.out.println("Soyadým :Hýrakapucu");
+        System.out.println("Yaþým :"14);
+        System.out.println("Okul No :16494");
+        System.out.println(" ");
+        syste.out.println();
+	}
+
+}
